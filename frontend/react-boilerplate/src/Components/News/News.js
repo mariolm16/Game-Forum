455e7baba18747b712011630792e7d7bbcb1caa0
mariolm16 <mariolm16@live.com>
2020-08-01 19:05:14 -0400

    able to call gamespot api and set state

--- frontend/react-boilerplate/src/Components/News/News.js
+++ frontend/react-boilerplate/src/Components/News/News.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 
+import { callGamespot } from "../../Service/api_helper";
+
 class News extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            news: ""
+            news: {}
         }
+    }
+
+    async componentDidMount() {
+        const news = await callGamespot();
+        this.setState({
+            news
+        })
     }
 
     render(props) {
@@ -14,6 +23,7 @@ class News extends Component {
             <div>
                 <h1>I am the news Component</h1>
                 <img src="https://upload.wikimedia.org/wikipedia/en/4/4f/Under_construction.JPG" alt="construction" />
+                <p>News provided by Gamespot.com</p>
             </div>
         )
     }
